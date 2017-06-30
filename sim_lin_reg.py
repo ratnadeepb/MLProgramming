bd22db4f069f802f4788d2aeba03584747772453
ratnadeepb <ratnadeep.bhattacharya1983@gmail.com>
2017-06-30 11:34:23 +0400

    Changing the program

--- sim_lin_reg.py
+++ sim_lin_reg.py
@@ -8,17 +8,17 @@ import sys
 if env not in sys.path:
     sys.path.append(env)
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import KFold
 from sklearn import metrics
 from process_v1 import process
 from model_score import adjusted_r_squared
+from sklearn.externals import joblib
 
-def sim_lin_reg(file, splits):
+def sim_lin_reg(file, save_to):
     """
     Simple Linear Regression
     :param file: File containing the data
-    :param file: Splits for K-Fold validation
-    :return: The adjusted R-squared score
+    :param save_to: File to which the model will be saved
+    :return: The adjusted R-squared score and the coefficients of the model
     """
 
     #################### Prepare the file ####################
@@ -32,6 +32,9 @@ def sim_lin_reg(file, splits):
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
 
+    #################### Saving the model ####################
+    joblib.dump(regressor, save_to)
+
     #################### Predicting ####################
     predictions = regressor.predict(X_test)
 
@@ -39,4 +42,18 @@ def sim_lin_reg(file, splits):
     r2_score = metrics.r2_score(y_test, predictions)
     adj_r2_score = adjusted_r_squared(X_test.shape[0], X_test.shape[1], r2_score)
 
-    return [regressor.coef_, adj_r2_score]
+    coeffs = [regressor.intercept_]
+    coeffs.extend(regressor.coef_)
+
+    return [coeffs, adj_r2_score]
+
+if __name__ == "__main__":
+    file = "/home/ratnadeepb/app/machine_learning/ml_programming/Data/Salary_Data.csv"
+    save_to = "/home/ratnadeepb/app/machine_learning/ml_programming/saved_models/sim_lin_reg.pkl"
+    l, adj_r2 = sim_lin_reg(file, save_to)
+
+    l2 = []
+    for i in l:
+        l2.append(round(i, 3))
+    print("The coefficient values are: ", l2)
+    print("The accurace of the model is: ", round(adj_r2, 4) * 100)
